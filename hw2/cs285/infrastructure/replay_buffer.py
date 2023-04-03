4ac1f69b7a17147208d8484254f1b69e5602f3a7
jongwoolee127 <britain88@gmail.com>
2023-04-02 17:55:47 -0700

    Debug path length error
    Added another condition for the while-loop to avoid calling more data than existing in the self.paths variable (type = list)
    
    However, we need to double check whether the previous process itself was wrong and passing wrong values to self.paths. Keep this in mind.

--- hw2/cs285/infrastructure/replay_buffer.py
+++ hw2/cs285/infrastructure/replay_buffer.py
@@ -71,7 +71,7 @@ class ReplayBuffer(object):
         return self.obs[rand_indices], self.acs[rand_indices], self.concatenated_rews[rand_indices], self.next_obs[rand_indices], self.terminals[rand_indices]
 
     def sample_recent_data(self, batch_size=1, concat_rew=True):
-        ipdb.set_trace()
+        # ipdb.set_trace()
 
         if concat_rew:
             return self.obs[-batch_size:], self.acs[-batch_size:], self.concatenated_rews[-batch_size:], self.next_obs[-batch_size:], self.terminals[-batch_size:]
@@ -79,7 +79,7 @@ class ReplayBuffer(object):
             num_recent_rollouts_to_return = 0
             num_datapoints_so_far = 0
             index = -1
-            while num_datapoints_so_far < batch_size:
+            while num_datapoints_so_far < batch_size and num_recent_rollouts_to_return < len(self.paths):
                 recent_rollout = self.paths[index]
                 index -=1
                 num_recent_rollouts_to_return +=1
